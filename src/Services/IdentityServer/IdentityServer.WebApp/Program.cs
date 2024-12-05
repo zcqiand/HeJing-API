7b73d157163c7f510a84d6810e56760204490961
zcqd <qiand@zcitc.com>
2024-12-05 12:45:43 +0800

    重构

--- src/Services/IdentityServer/IdentityServer.WebApp/Program.cs
+++ src/Services/IdentityServer/IdentityServer.WebApp/Program.cs
@@ -1,98 +1,25 @@
-﻿using CommonServer.Infrastructure;
-using IdentityServer.WebApp;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Quartz;
-
 var builder = WebApplication.CreateBuilder(args);
 
-var configuration = builder.Configuration;
-var services = builder.Services;
-
-services.AddControllersWithViews();
-services.AddRazorPages();
-
-services.AddCors(options =>
-{
-    options.AddDefaultPolicy(builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyHeader()
-               .AllowAnyMethod();
-    });
-});
-
-services.AddQuartz(options =>
-{
-    options.UseMicrosoftDependencyInjectionJobFactory();
-    options.UseSimpleTypeLoader();
-    options.UseInMemoryStore();
-});
-
-services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
-
-var connectionString = configuration.GetConnectionString("CommonServerDbConnection") ?? throw new InvalidOperationException("Connection string 'CommonServerDbConnection' not found.");
-
-services.AddDbContext<CommonServerDbContext>(options =>
-{
-    options.EnableSensitiveDataLogging(true);
-    options.UseNpgsql(connectionString);
-
-    options.UseOpenIddict();
-});
-
-services.AddOpenIddict()
-    .AddCore(options =>
-    {
-        options.UseEntityFrameworkCore()
-               .UseDbContext<CommonServerDbContext>();
-    })
-    .AddServer(options =>
-    {
-        options.SetAuthorizationEndpointUris("connect/authorize")
-                .SetIntrospectionEndpointUris("connect/introspect")
-                .SetTokenEndpointUris("connect/token");
-
-        options.AllowAuthorizationCodeFlow()
-            .AllowRefreshTokenFlow();
-
-        options.AddEncryptionKey(new SymmetricSecurityKey(
-            Convert.FromBase64String("DRjd/GnduI3Efzen9V9BvbNUfc/VKgXltV7Kbk9sMkY=")));
-
-        options.AddDevelopmentSigningCertificate();
-
-        options.UseAspNetCore()
-               .EnableAuthorizationEndpointPassthrough();
-    })
-    .AddValidation(options =>
-    {
-        options.UseLocalServer();
-
-        options.UseAspNetCore();
-    });
-
-services.AddAuthorization()
-    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie();
-
-services.AddHostedService<Worker>();
-
-services.AddAuthorization();
+// Add services to the container.
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-app.UseCors();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseAuthentication();
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
